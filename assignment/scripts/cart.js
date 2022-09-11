f7d2d73d7c4de2066c2fab3fe81efcdf0272644e
Maia Johnson <maiaj1306@gmail.com>
2022-09-11 03:16:34 -0500

    created const maxItems

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -46,3 +46,24 @@ function empty() {
 
 empty();
 console.log(basket);
+
+// STRETCH GOALS
+// Add a global const named maxItems and set it to 5.
+const maxItems = 5;
+
+//Create a function called isFull(). It should:
+    // return false if the basket contains less than max number of items
+    // return true otherwise (equal or more than maxItems)
+
+
+// Update the required addItem function to:
+    // Use the isFull function to prevent more than maxItems from being added to the basket.
+    // If an item was added to the array, return true
+    // If there was no room and the item could not be added return false
+
+
+// Create a function called removeItem. It should:
+    // Take an input parameter for a string item
+    // Use Array.indexOf to find the index of the first matching item in the basket.
+    // Use Array.splice to remove the first matching item from the basket.
+    // Return the item removed or null if the item was not found
